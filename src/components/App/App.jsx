3c3340db1c562ce2dbe3f8b62be30b61165508c4
Konstantin Retunsky <konstantin.retunsky@gmail.com>
2021-06-11 19:07:35 +0500

    mv App to components dir

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import './App.css';
+
 function App() {
 	return (
-		<div className="App">
-			sadf
-		</div>
+    <div className="App">
+    </div>
 	);
 }
-export default App
+export default App;
